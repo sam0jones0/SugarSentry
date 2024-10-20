98e785bbf7a3c8947a1a388ec2b6bcf0e7a371f5
Sam Jones <sam0jones0@gmail.com>
2024-10-20 10:30:56 +0100

    print in mmol

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,3 @@
-
-
-// #include <WiFiClientSecure.h>
-// #include <dexcom_constants.h>
-
-// const char *ssid = "";
-// const char *password = "";
-
-// const char *server = DexcomConst::DEXCOM_BASE_URL_OUS; // Server URL
-
-// www.howsmyssl.com root certificate authority, to verify the server
-// change it to your server root CA
-// SHA1 fingerprint is broken now!
-
-// You can use x.509 client certificates if you want
-// const char* test_client_key = "";   //to verify the client
-// const char* test_client_cert = "";  //to verify the client
-
-// WiFiClientSecure client;
-
-// void setup()
-// {
-//   // Initialize serial and wait for port to open:
-//   Serial.begin(115200);
-//   delay(100);
-
-//   Serial.print("Attempting to connect to SSID: ");
-//   Serial.println(ssid);
-//   WiFi.begin(ssid, password);
-
-//   // attempt to connect to Wifi network:
-//   while (WiFi.status() != WL_CONNECTED)
-//   {
-//     Serial.print(".");
-//     // wait 1 second for re-trying
-//     delay(1000);
-//   }
-
-//   Serial.print("Connected to ");
-//   Serial.println(ssid);
-
-//   client.setCACert(DexcomConst::rootCA);
-//   // client.setCertificate(test_client_cert); // for client verification
-//   // client.setPrivateKey(test_client_key);	// for client verification
-
-//   Serial.println("\nStarting connection to server...");
-//   if (!client.connect(server, 443))
-//     Serial.println("Connection failed!");
-//   else
-//   {
-//     Serial.println("Connected to server!");
-//     // Make a HTTP request:
-//     client.println("GET https://shareous1.dexcom.com/General/LoginPublisherAccountById HTTP/1.0");
-//     client.println("Host: shareous1.dexcom");
-//     client.println("Connection: close");
-//     client.println();
-
-//     while (client.connected())
-//     {
-//       String line = client.readStringUntil('\n');
-//       if (line == "\r")
-//       {
-//         Serial.println("headers received");
-//         break;
-//       }
-//     }
-//     // if there are incoming bytes available
-//     // from the server, read them and print them:
-//     while (client.available())
-//     {
-//       char c = client.read();
-//       Serial.write(c);
-//     }
-
-//     client.stop();
-//   }
-// }
-
-// void loop()
-// {
-//   // do nothing
-// }
-
-// // // // // // // //
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include "config.h"
@@ -177,8 +92,8 @@ void fetchAndPrintGlucoseReading(DexcomClient &dexcomClient)
     if (reading)
     {
       Serial.print("Last glucose reading: ");
-      Serial.print(reading->getValue());
-      Serial.println(" mg/dL");
+      Serial.print(reading->getMmolL());
+      Serial.println(" mmol/L");
     }
     else
     {
